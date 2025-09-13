a010f1ec91b85f308b5511530148792512f49bdf
Asir <rezwon0987@gmail.com>
2025-09-13 23:30:39 +0600

    gg

--- messmanagement/Form1.Designer.cs
+++ messmanagement/Form1.Designer.cs
@@ -48,9 +48,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(48, 122);
+            this.label1.Location = new System.Drawing.Point(32, 79);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(161, 29);
+            this.label1.Size = new System.Drawing.Size(107, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Welcome To";
             // 
@@ -58,18 +59,20 @@
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(58, 151);
+            this.label2.Location = new System.Drawing.Point(39, 98);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(114, 20);
+            this.label2.Size = new System.Drawing.Size(77, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Mess Manager";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(49, 184);
+            this.label3.Location = new System.Drawing.Point(33, 120);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(145, 20);
+            this.label3.Size = new System.Drawing.Size(96, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "Your email address";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -77,25 +80,28 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(49, 247);
+            this.label4.Location = new System.Drawing.Point(33, 161);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(159, 20);
+            this.label4.Size = new System.Drawing.Size(106, 13);
             this.label4.TabIndex = 4;
             this.label4.Text = "Enter Your Password\r\n";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(52, 207);
+            this.textBox1.Location = new System.Drawing.Point(35, 135);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(408, 26);
+            this.textBox1.Size = new System.Drawing.Size(273, 20);
             this.textBox1.TabIndex = 5;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(52, 270);
+            this.textBox2.Location = new System.Drawing.Point(35, 176);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(408, 26);
+            this.textBox2.Size = new System.Drawing.Size(273, 20);
             this.textBox2.TabIndex = 6;
             // 
             // button1
@@ -103,9 +109,10 @@
             this.button1.BackColor = System.Drawing.Color.Red;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(52, 312);
+            this.button1.Location = new System.Drawing.Point(35, 203);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(408, 34);
+            this.button1.Size = new System.Drawing.Size(272, 22);
             this.button1.TabIndex = 7;
             this.button1.Text = "Login Now\r\n";
             this.button1.UseVisualStyleBackColor = false;
@@ -114,9 +121,10 @@
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(322, 349);
+            this.linkLabel1.Location = new System.Drawing.Point(215, 227);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(138, 20);
+            this.linkLabel1.Size = new System.Drawing.Size(92, 13);
             this.linkLabel1.TabIndex = 8;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Forgot Password?\r\n";
@@ -124,18 +132,20 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(245, 384);
+            this.label5.Location = new System.Drawing.Point(163, 250);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(23, 20);
+            this.label5.Size = new System.Drawing.Size(16, 13);
             this.label5.TabIndex = 9;
             this.label5.Text = "or";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::messmanagement.Properties.Resources.project_picture;
-            this.pictureBox1.Location = new System.Drawing.Point(62, 40);
+            this.pictureBox1.Location = new System.Drawing.Point(41, 26);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(88, 79);
+            this.pictureBox1.Size = new System.Drawing.Size(59, 51);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
@@ -143,9 +153,10 @@
             // button2
             // 
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(52, 407);
+            this.button2.Location = new System.Drawing.Point(35, 265);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(408, 40);
+            this.button2.Size = new System.Drawing.Size(272, 26);
             this.button2.TabIndex = 10;
             this.button2.Text = "Continue with Google";
             this.button2.UseVisualStyleBackColor = true;
@@ -153,18 +164,20 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(245, 460);
+            this.label6.Location = new System.Drawing.Point(163, 299);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(23, 20);
+            this.label6.Size = new System.Drawing.Size(16, 13);
             this.label6.TabIndex = 11;
             this.label6.Text = "or";
             // 
             // linkLabel2
             // 
             this.linkLabel2.AutoSize = true;
-            this.linkLabel2.Location = new System.Drawing.Point(132, 489);
+            this.linkLabel2.Location = new System.Drawing.Point(88, 318);
+            this.linkLabel2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.linkLabel2.Name = "linkLabel2";
-            this.linkLabel2.Size = new System.Drawing.Size(239, 20);
+            this.linkLabel2.Size = new System.Drawing.Size(164, 13);
             this.linkLabel2.TabIndex = 12;
             this.linkLabel2.TabStop = true;
             this.linkLabel2.Text = "New here? Create Account Now.";
@@ -172,9 +185,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1440, 567);
+            this.ClientSize = new System.Drawing.Size(369, 369);
             this.Controls.Add(this.linkLabel2);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.button2);
@@ -188,6 +201,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
