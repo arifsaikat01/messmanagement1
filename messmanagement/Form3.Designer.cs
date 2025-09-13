2f054908d7cb6949cb4bc6e368ba89016185e34b
Asir <rezwon0987@gmail.com>
2025-09-13 22:52:33 +0600

    fsf

--- messmanagement/Form3.Designer.cs
+++ messmanagement/Form3.Designer.cs
@@ -945,7 +945,7 @@ namespace messmanagement
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(112, 51);
+            this.label1.Location = new System.Drawing.Point(23, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
